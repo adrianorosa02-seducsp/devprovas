de567213692e207ec813e3aec8d5e343189361b6
Adriano Rosa <adrianorosa02@prof.educacao.sp.gov.br>
2026-06-24 18:08:44 -0300

    Feat:Ajuste disponibilizando stack devprovas.yaml

--- app/core/database.py
+++ app/core/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/devprovas"
+DATABASE_URL = "postgresql://appuser:appsenha@db.inetz.com.br:5432/appdb"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
